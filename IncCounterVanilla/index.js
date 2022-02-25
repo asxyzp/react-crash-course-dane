a1c107ed473b8dbc7123b106af806fc14d84f4a2
Aashish Loknath Panigrahi <aashish@psytech.ai>
2022-02-26 00:49:09 +0530

    + MINOR: Adding additional comments for readability + Adding Bootstrap classes for styling

--- IncCounterVanilla/index.js
+++ IncCounterVanilla/index.js
@@ -1,7 +1,12 @@
+// STORES THE CURRENT COUNT
 let currentCount = 0;
+
+// DOM NODES
 const countDisplay = document.getElementById("count-display");
 const incrementBtn = document.getElementById("increment-btn");
-incrementBtn.addEventListener("click",(event)=>{
-    currentCount+=1;
+
+// BUTTON CLICK EVENT HANDLER
+incrementBtn.addEventListener("click", (event) => {
+    currentCount += 1;
     countDisplay.innerHTML = currentCount;
 });
